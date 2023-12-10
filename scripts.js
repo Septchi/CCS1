2550ce4427c73430f37278070055f3ba7d2d5c1c
Septchi <greenenji@gmail.com>
2023-12-11 07:04:37 +0800

    fixed navbar and dynamic latest click function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const latest = document.getElementById("latest");
+let latest = document.getElementById("latest");
 const colors = ["powderblue", "salmon", "palevioletred"];
 fetch("./latest.json").then(res=>res.json()).then(data=> {
   for(let [i, entry] of data.entry.entries()){
@@ -7,20 +7,47 @@ fetch("./latest.json").then(res=>res.json()).then(data=> {
     let img = document.createElement("img");
     img.src = entry.img;
     let p = document.createElement("p");
+    let b = document.createElement("b");
     let n = document.createTextNode(entry.p);
+    b.appendChild(document.createTextNode(entry.b));
+    p.appendChild(b);
+    p.appendChild(document.createElement("br"));
     p.appendChild(n);
     div.appendChild(img);
     div.appendChild(p);
     latest.appendChild(div);
+    div.addEventListener("click", function(evt){
+      let element = evt.target.tagName == 'DIV'  ? evt.target : evt.target.parentNode;
+      element.querySelector("p").style.display = window.getComputedStyle(element.querySelector("p")).display == 'none' ? "block" : "none";
+    });
     
   }
 });
+// fetch("./latest.json").then(res=>res.json()).then(data=> {
+//   for(let [i, entry] of data.entry.entries()){
+//     let div = document.createElement("div");
+//     div.style.background = colors[i%colors.length];
+//     let img = document.createElement("img");
+//     img.src = entry.img;
+//     let p = document.createElement("p");
+//     let n = document.createTextNode(entry.p);
+//     p.appendChild(n);
+//     div.appendChild(img);
+//     div.appendChild(p);
+//     latest.appendChild(div);
+//     
+//   }
+// });
 
 latest.addEventListener("wheel", function (evt){
 	latest.scrollLeft += evt.deltaY/2;
 	evt.preventDefault();
 });
-for (let item of latest.querySelectorAll("div")){
+console.log(latest.childNodes);
+const children = latest.childNodes;
+console.log("hello");
+for (let item of children){
+  console.log("hello");
 	item.addEventListener("click", function(evt){
     let element = evt.target.tagName == 'DIV'  ? evt.target : evt.target.parentNode;
     element.querySelector("p").style.display = window.getComputedStyle(element.querySelector("p")).display == 'none' ? "block" : "none";
