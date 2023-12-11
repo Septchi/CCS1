8127ea0abf2d4d8d781bde5dc667c699463a5f17
Septchi <greenenji@gmail.com>
2023-12-11 11:19:11 +0800

    added new js and json files

---
+++ news.js
@@ -0,0 +1,27 @@
+const normal = document.getElementById("normal");
+
+fetch("./news.json").then(res=>res.json()).then(data=> {
+  for(let [i, entry] of data.entry.entries()){
+    let div = document.createElement("div");
+    let h1 = document.createElement("H1");
+    h1.innerHTML = entry.title;
+    let img = document.createElement("img");
+    img.src = entry.img;
+    let p = document.createElement("p");
+    p.innerHTML = entry.text;
+    let d = document.createElement("p");
+    d.innerHTML = entry.date;
+    div.appendChild(h1);
+    let body = document.createElement("div");
+    body.classList.add("body");
+    body.appendChild(img);
+    let text = document.createElement("dev");
+    text.classList.add("text");
+    text.appendChild(p);
+    text.appendChild(d);
+    body.appendChild(text);
+    div.appendChild(body);
+    normal.appendChild(div);
+    
+  }
+});
