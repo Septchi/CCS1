78fe58f526ce4c11b5faa752d61d7392c83ae0b5
Septchi <greenenji@gmail.com>
2023-12-15 09:03:39 +0800

    glitch

--- public/index.php
+++ public/index.php
@@ -1,1 +1,25 @@
-<?php phpinfo();
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Nintendo Alert</title>
+		<link rel="stylesheet" type="text/css" href="reviews.css">
+		<link rel="stylesheet" type="text/css" href="home.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	</head>
+	<body> 
+    <?php
+      include_once 'header.php';
+    ?>
+    <img id="titlePage" src="images/Title Design Page.png">
+    <h1 style="text-align: center; margin-top: 5%;">Check out the latest news or reviews right now!</h1>
+		<div id="latest"></div>
+		
+		<div id="bottom">
+			<h4>NintendoAlert 2023| operated by GamingNewshounds</h3>
+			<button onclick="location.href='mailto:gamingnewshounds@gmail.com'">
+			</button>
+			<h4>Contact Us | gamingnewshounds@gmail.com</h3>
+		</div>
+		<script type='module' src="scripts.js"></script>
+	</body>
+</html>
